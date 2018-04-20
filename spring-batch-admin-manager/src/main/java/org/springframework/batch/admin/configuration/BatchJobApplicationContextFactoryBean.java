af24ad5246bf00c85e964e5d9672f2d1245d0e14
Jonathan Keller <keller.jonathan@gmail.com>
2018-04-20 15:29:54 -0700

    Got sample server to start up with new Jetty and config property.

---
+++ spring-batch-admin-manager/src/main/java/org/springframework/batch/admin/configuration/BatchJobApplicationContextFactoryBean.java
@@ -0,0 +1,7 @@
+package org.springframework.batch.admin.configuration;
+
+import org.springframework.batch.core.configuration.support.ClasspathXmlApplicationContextsFactoryBean;
+
+public class BatchJobApplicationContextFactoryBean extends ClasspathXmlApplicationContextsFactoryBean {
+
+}
